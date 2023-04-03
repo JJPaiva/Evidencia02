fdcd96f4391e7a2de505cee1cbca75c7e91e47f9
L1TnzO <59234471+L1TnzO@users.noreply.github.com>
2023-04-03 10:38:31 -0400

    Update and rename HolaMundoJuan.java to HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,4 +1,4 @@
-public class HolaMundoJuan{
+public class HolaMundo{
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Juan Paiva");
